e7464182b4472d4086ba96dd33e8190bcfaa97e5
Freddie Ridell <freddie@codogo.io>
2017-11-29 11:23:49 +0000

    fix(priorty): only allow single lowercase letters for priority

--- src/parseDefinedProps.js
+++ src/parseDefinedProps.js
@@ -3,7 +3,7 @@ import { parse, } from "./dateShortcuts";
 export default ({ due, wait, depends, project, priority, recur, ...rest }) => ({
 	depends,
 	due: due ? parse(due).toISOString() : null,
-	priority,
+	priority: priority ? priority.slice(0, 1).toLowerCase() : null,
 	project,
 	recur,
 	...rest,
